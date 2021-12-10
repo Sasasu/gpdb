e4bc2a08fc972d63890fe976b189a9a772f697ac
Sasasu <i@sasa.su>
2021-12-10 16:03:08 +0800

    Enable pg_trgm by default
    pg_trgm is a build-in extension of PostgreSQL. this patch enables it in Greenplum.
    
    pg_trgm does not contain new file formats or executor changes, which has good compatibility with Greenplum.
    
    Signed-off-by: Sasasu <i@sasa.su>

--- contrib/pg_trgm/sql/pg_trgm.sql
+++ contrib/pg_trgm/sql/pg_trgm.sql
@@ -1,3 +1,4 @@
+set optimizer=off;
 CREATE EXTENSION pg_trgm;
 
 select show_trgm('');
@@ -13,7 +14,7 @@ select similarity('wow',' WOW ');
 
 select similarity('---', '####---');
 
-CREATE TABLE test_trgm(t text);
+CREATE TABLE test_trgm(t text) DISTRIBUTED BY (t);
 
 \copy test_trgm from 'data/trgm.data'
 
@@ -43,7 +44,7 @@ select t,similarity(t,'gwertyu0988') as sml from test_trgm where t % 'gwertyu098
 select t,similarity(t,'gwertyu1988') as sml from test_trgm where t % 'gwertyu1988' order by sml desc, t;
 select count(*) from test_trgm where t ~ '[qwerty]{2}-?[qwerty]{2}';
 
-create table test2(t text);
+create table test2(t text) distributed by (t);
 insert into test2 values ('abcdef');
 insert into test2 values ('quark');
 insert into test2 values ('  z foo bar');
@@ -54,36 +55,36 @@ explain (costs off)
   select * from test2 where t like '%BCD%';
 explain (costs off)
   select * from test2 where t ilike '%BCD%';
-select * from test2 where t like '%BCD%';
-select * from test2 where t like '%bcd%';
-select * from test2 where t like E'%\\bcd%';
-select * from test2 where t ilike '%BCD%';
-select * from test2 where t ilike 'qua%';
-select * from test2 where t like '%z foo bar%';
-select * from test2 where t like '  z foo%';
+select * from test2 where t like '%BCD%' order by t;
+select * from test2 where t like '%bcd%' order by t;
+select * from test2 where t like E'%\\bcd%' order by t;
+select * from test2 where t ilike '%BCD%' order by t;
+select * from test2 where t ilike 'qua%' order by t;
+select * from test2 where t like '%z foo bar%' order by t;
+select * from test2 where t like '  z foo%' order by t;
 explain (costs off)
   select * from test2 where t ~ '[abc]{3}';
 explain (costs off)
   select * from test2 where t ~* 'DEF';
-select * from test2 where t ~ '[abc]{3}';
-select * from test2 where t ~ 'a[bc]+d';
-select * from test2 where t ~ '(abc)*$';
-select * from test2 where t ~* 'DEF';
-select * from test2 where t ~ 'dEf';
-select * from test2 where t ~* '^q';
-select * from test2 where t ~* '[abc]{3}[def]{3}';
-select * from test2 where t ~* 'ab[a-z]{3}';
-select * from test2 where t ~* '(^| )qua';
-select * from test2 where t ~ 'q.*rk$';
-select * from test2 where t ~ 'q';
-select * from test2 where t ~ '[a-z]{3}';
-select * from test2 where t ~* '(a{10}|b{10}|c{10}){10}';
-select * from test2 where t ~ 'z foo bar';
-select * from test2 where t ~ ' z foo bar';
-select * from test2 where t ~ '  z foo bar';
-select * from test2 where t ~ '  z foo';
-select * from test2 where t ~ 'qua(?!foo)';
-select * from test2 where t ~ '/\d+/-\d';
+select * from test2 where t ~ '[abc]{3}' order by t;
+select * from test2 where t ~ 'a[bc]+d' order by t;
+select * from test2 where t ~ '(abc)*$' order by t;
+select * from test2 where t ~* 'DEF' order by t;
+select * from test2 where t ~ 'dEf' order by t;
+select * from test2 where t ~* '^q' order by t;
+select * from test2 where t ~* '[abc]{3}[def]{3}' order by t;
+select * from test2 where t ~* 'ab[a-z]{3}' order by t;
+select * from test2 where t ~* '(^| )qua' order by t;
+select * from test2 where t ~ 'q.*rk$' order by t;
+select * from test2 where t ~ 'q' order by t;
+select * from test2 where t ~ '[a-z]{3}' order by t;
+select * from test2 where t ~* '(a{10}|b{10}|c{10}){10}' order by t;
+select * from test2 where t ~ 'z foo bar' order by t;
+select * from test2 where t ~ ' z foo bar' order by t;
+select * from test2 where t ~ '  z foo bar' order by t;
+select * from test2 where t ~ '  z foo' order by t;
+select * from test2 where t ~ 'qua(?!foo)' order by t;
+select * from test2 where t ~ '/\d+/-\d' order by t;
 drop index test2_idx_gin;
 
 create index test2_idx_gist on test2 using gist (t gist_trgm_ops);
@@ -92,33 +93,33 @@ explain (costs off)
   select * from test2 where t like '%BCD%';
 explain (costs off)
   select * from test2 where t ilike '%BCD%';
-select * from test2 where t like '%BCD%';
-select * from test2 where t like '%bcd%';
-select * from test2 where t like E'%\\bcd%';
-select * from test2 where t ilike '%BCD%';
-select * from test2 where t ilike 'qua%';
-select * from test2 where t like '%z foo bar%';
-select * from test2 where t like '  z foo%';
+select * from test2 where t like '%BCD%' order by t;
+select * from test2 where t like '%bcd%' order by t;
+select * from test2 where t like E'%\\bcd%' order by t;
+select * from test2 where t ilike '%BCD%' order by t;
+select * from test2 where t ilike 'qua%' order by t;
+select * from test2 where t like '%z foo bar%' order by t;
+select * from test2 where t like '  z foo%' order by t;
 explain (costs off)
   select * from test2 where t ~ '[abc]{3}';
 explain (costs off)
   select * from test2 where t ~* 'DEF';
-select * from test2 where t ~ '[abc]{3}';
-select * from test2 where t ~ 'a[bc]+d';
-select * from test2 where t ~ '(abc)*$';
-select * from test2 where t ~* 'DEF';
-select * from test2 where t ~ 'dEf';
-select * from test2 where t ~* '^q';
-select * from test2 where t ~* '[abc]{3}[def]{3}';
-select * from test2 where t ~* 'ab[a-z]{3}';
-select * from test2 where t ~* '(^| )qua';
-select * from test2 where t ~ 'q.*rk$';
-select * from test2 where t ~ 'q';
-select * from test2 where t ~ '[a-z]{3}';
-select * from test2 where t ~* '(a{10}|b{10}|c{10}){10}';
-select * from test2 where t ~ 'z foo bar';
-select * from test2 where t ~ ' z foo bar';
-select * from test2 where t ~ '  z foo bar';
-select * from test2 where t ~ '  z foo';
-select * from test2 where t ~ 'qua(?!foo)';
-select * from test2 where t ~ '/\d+/-\d';
+select * from test2 where t ~ '[abc]{3}' order by t;
+select * from test2 where t ~ 'a[bc]+d' order by t;
+select * from test2 where t ~ '(abc)*$' order by t;
+select * from test2 where t ~* 'DEF' order by t;
+select * from test2 where t ~ 'dEf' order by t;
+select * from test2 where t ~* '^q' order by t;
+select * from test2 where t ~* '[abc]{3}[def]{3}' order by t;
+select * from test2 where t ~* 'ab[a-z]{3}' order by t;
+select * from test2 where t ~* '(^| )qua' order by t;
+select * from test2 where t ~ 'q.*rk$' order by t;
+select * from test2 where t ~ 'q' order by t;
+select * from test2 where t ~ '[a-z]{3}' order by t;
+select * from test2 where t ~* '(a{10}|b{10}|c{10}){10}' order by t;
+select * from test2 where t ~ 'z foo bar' order by t;
+select * from test2 where t ~ ' z foo bar' order by t;
+select * from test2 where t ~ '  z foo bar' order by t;
+select * from test2 where t ~ '  z foo' order by t;
+select * from test2 where t ~ 'qua(?!foo)' order by t;
+select * from test2 where t ~ '/\d+/-\d' order by t;
