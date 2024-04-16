42bde5bb717c46da12af582cb32b2eecb113fa1c
Sasasu <i@sasa.su>
2024-04-16 23:43:11 +0800

    front

--- src/include/fe_utils/frontend_hook.h
+++ src/include/fe_utils/frontend_hook.h
@@ -4,11 +4,6 @@
 #define FRONTEND
 #endif
 
+extern char pg_install_path[], pg_data_path[];
 
-extern char *pg_install_path;
-extern char *pg_data_path;
-
-extern void add_library_to_load(const char *lib);
-
-// load the fronted libraries
-extern void frontend_load_libraries(const char *procname);
+void frontend_load_libraries(const char *procname);
